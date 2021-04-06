3dc56b0589e47be0846420f5074cdb4fe88b3a55
snisha24 <77018226+snisha24@users.noreply.github.com>
2021-04-06 21:32:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+menu_list_array = ["Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
+    
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+     htmldata=htmldata+"</ol>"
+     document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    getmenu();
+
+}
